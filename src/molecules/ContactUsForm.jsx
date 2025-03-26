ee21809437c7ee0db83033a025ca8e09f86ef5e0
Neeraj Prajapati <neerajkiet@gmail.com>
2025-03-26 12:55:39 +0530

    contact us script url fixed , new deployment for sheet done and url changed to contact us funtion

--- src/molecules/ContactUsForm.jsx
+++ src/molecules/ContactUsForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbww_tEhVzKLtZEWTce_dx9TYrDAmrDj_Wz2HkgvHJnIIQ4Ji38-qjqTFKlWA3EpkuoRnw/exec"
 
 function ContactForm() {
     const [formData, setFormData] = useState({
@@ -31,7 +32,7 @@ function ContactForm() {
         setError("");
         try {
             const response = await fetch(
-                "https://script.google.com/macros/s/AKfycbwB7wqJNALTGKFva3QK_w0h57f118AAN77dBOr78ye_-jm8N8ISQdfgrOvRT0FnL8Va/exec", // Replace with your actual URL
+                SHEET_URL, // Replace with your actual URL
                 {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
@@ -39,9 +40,12 @@ function ContactForm() {
                     mode: "no-cors"
                 }
             );
+            console.log("response",response)
             setSuccess(true);
             setFormData({ fullName: "", mobile: "", email: "" });
         } catch (err) {
+            console.log("err",err)
+
             setError("Network error. Please try again.");
         }
     };
