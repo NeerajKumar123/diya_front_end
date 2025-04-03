fd1cb1e0c7123f7407b6e7600da7ae8d01e18827
Neeraj Prajapati <neerajkiet@gmail.com>
2025-04-03 11:29:29 +0530

    socialbar changes done

--- src/molecules/SocialBar.jsx
+++ src/molecules/SocialBar.jsx
@@ -6,7 +6,7 @@ const SocialBar = () => {
         
         {/* Right-aligned social icons using images */}
         <div className="flex space-x-4">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/share/15vkNso9V8/" target="_blank" rel="noopener noreferrer">
             <img src="/fb.svg" alt="Facebook" className="w-5 h-5" />
           </a> 
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
@@ -15,7 +15,7 @@ const SocialBar = () => {
           <a href="https://www.youtube.com/@diyasamajiksanstha3883/featured" target="_blank" rel="noopener noreferrer">
             <img src="/youtube.svg" alt="YouTube" className="w-5 h-5" />
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/diya-samajik-sanstha-66594514a/" target="_blank" rel="noopener noreferrer">
             <img src="/linkedin.svg" alt="YouTube" className="w-5 h-5" />
           </a>
         </div>
